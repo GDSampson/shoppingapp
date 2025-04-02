a6aa2e98587dd618cf8605cefe094ed501298924
Daniel Sampson <sampsondg@outlook.com>
2025-04-02 11:42:22 -0300

    setup tanstack query client

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,29 @@
+import { useReactQueryDevTools } from "@dev-plugins/react-query";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Stack } from "expo-router";
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 60 * 1000, // 1 minute stale time (queries would be cached for this long)
+    },
+  },
+});
+
 export default function RootLayout() {
-  return <Stack />;
+  useReactQueryDevTools(queryClient);
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Stack>
+        <Stack.Screen name="index"
+          options={{
+            title: 'Only Dans',
+            headerShadowVisible: false,
+            headerTitleAlign: 'center',
+          }}
+        />
+      </Stack>
+    </QueryClientProvider>
+  );
 }
