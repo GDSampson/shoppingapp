d6ec9afd5dc9adccfc86cae1c9906eada7b59efe
Daniel Sampson <sampsondg@outlook.com>
2025-04-02 11:42:47 -0300

    setup tanstack queries to get the api data for categories and products

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,22 @@
+import { getCategories, getProducts } from "@/utils/api";
+import { useQuery } from "@tanstack/react-query";
 import { Text, View } from "react-native";
 
 export default function Index() {
+
+  const {data: products} = useQuery( { // grab the products with useQuery hook
+    queryKey: ['products'], //store result in the products key
+    queryFn: getProducts, //use the getProducts function from api.ts 
+  }) 
+
+
+  const {data: categories = []} = useQuery( { 
+    queryKey: ['categories'], 
+    queryFn: getCategories, 
+  }) 
+
+  const allCategories = ['all', ...categories]
+
   return (
     <View
       style={{
