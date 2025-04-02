426972d192db3adf9dac36f90b7a7d0fcc894a08
Daniel Sampson <sampsondg@outlook.com>
2025-04-02 14:12:14 -0300

    add category selection and filtering products based on selected category - add searchbar - currently only filters when 'all' cat is selected

--- app/index.tsx
+++ app/index.tsx
@@ -1,37 +1,72 @@
 import ProductCard from "@/components/ProductCard";
 import { getCategories, getProducts, Product } from "@/utils/api";
+import { COLORS } from "@/utils/colors";
 import { FlashList } from "@shopify/flash-list";
 import { useQuery } from "@tanstack/react-query";
-import { useCallback } from "react";
-import { Text, View, StyleSheet } from "react-native";
+import { Stack } from "expo-router";
+import { useCallback, useMemo, useState } from "react";
+import { Text, View, StyleSheet, ScrollView, Pressable, Platform } from "react-native";
+import {useHeaderHeight} from '@react-navigation/elements';
 
 export default function Index() {
+  const [selectedCategory, setSelectedCategory] = useState<string>('all');
+  const [searchQuery, setSearchQuery] = useState<string>('');
+  const headerHeight = useHeaderHeight();
 
-  const {data: products, isLoading, refetch, isRefetching} = useQuery( { // grab the products with useQuery hook
+  const { data: products, isLoading, refetch, isRefetching } = useQuery({ // grab the products with useQuery hook
     queryKey: ['products'], //store result in the products key
     queryFn: getProducts, //use the getProducts function from api.ts 
-  }) 
+  })
 
 
-  const {data: categories = []} = useQuery( { 
-    queryKey: ['categories'], 
-    queryFn: getCategories, 
-  }) 
+  const { data: categories = [] } = useQuery({
+    queryKey: ['categories'],
+    queryFn: getCategories,
+  })
 
   const allCategories = ['all', ...categories]
 
-  const renderProduct = useCallback(({item}: {item: Product}) => {
+  const filteredProducts = useMemo(() => {
+    return products?.filter((product) => {
+      if (selectedCategory !== 'all') {
+        return product.category === selectedCategory;
+      }
+      return product.title.toLowerCase().includes(searchQuery.toLowerCase());
+    })
+  }, [products, searchQuery, selectedCategory]);
+
+  const renderProduct = useCallback(({ item }: { item: Product }) => {
     return <ProductCard product={item} /> // the product prop in the component will store the item 
   }, []);
 
   return (
-    <View style={styles.container}>
-      <FlashList 
-        data={products}
+    <View style={[styles.container, {marginTop: Platform.select({ios: headerHeight, android: 0})} ]}>
+      <Stack.Screen
+        options={{headerSearchBarOptions: {onChangeText: 
+          (e) => setSearchQuery(e.nativeEvent.text)}}}
+      />
+      <View style={styles.categoryContainer}>
+        <ScrollView style={styles.categoryScrollView} horizontal showsHorizontalScrollIndicator={false}>
+          {allCategories.map((category) => (
+            <Pressable
+              key={category}
+              style={[styles.categoryButton, 
+              selectedCategory === category && styles.selectedCategory,]}
+              onPress={() => setSelectedCategory(category)}
+            >
+              <Text style={[styles.categoryButtonText,
+                selectedCategory === category && styles.selectedCategoryText
+               ]}>{category.charAt(0).toUpperCase() + category.slice(1)}</Text>
+            </Pressable>
+          ))}
+        </ScrollView>
+      </View>
+      <FlashList
+        data={filteredProducts}
         renderItem={renderProduct}
         estimatedItemSize={200}
         numColumns={2}
-        contentContainerStyle={ { padding: 8}}
+        contentContainerStyle={{ padding: 8 }}
         keyExtractor={(item) => item.id.toString()}
         onRefresh={refetch}
         refreshing={isRefetching}
@@ -45,4 +80,32 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
   },
+  categoryContainer: {
+    height: 60,
+    zIndex:1,
+    boxShadow: '0 0 10px rgba(0, 0, 0, .3)',
+  },
+  categoryScrollView:{
+    paddingHorizontal: 10,
+  },
+  categoryButton: {
+    paddingHorizontal: 16,
+    paddingVertical: 8,
+    borderRadius: 20,
+    marginHorizontal: 4,
+    backgroundColor: '#f0f0f0',
+    alignSelf: 'center',
+
+
+  },
+  categoryButtonText: {
+    fontSize: 14,
+    color: '#666',
+  },
+  selectedCategory: {
+    backgroundColor: COLORS.primary,
+  },
+  selectedCategoryText: {
+    color:'#fff',
+  }
 })
