3ea74b59a18be7418571279496bd31b181bb5ba2
Daniel Sampson <sampsondg@outlook.com>
2025-04-02 16:09:02 -0300

    add functionality to add and reduce products from cart state - add clearCart functionality (isn't persisting between reloads)

--- store/cartStore.ts
+++ store/cartStore.ts
@@ -1,8 +1,8 @@
 import { Product } from "@/utils/api";
-import {create} from 'zustand';
+import { create } from 'zustand';
 
 export interface CartState {
-    products: Array<Product & {quantity: number}>;
+    products: Array<Product & { quantity: number }>;
     addProduct: (product: Product) => void;
     reduceProudct: (product: Product) => void;
     clearCart: () => void;
@@ -18,9 +18,61 @@ const INITIAL_STATE = {
 
 const useCartStore = create<CartState>((set, get) => ({
     ...INITIAL_STATE,
-    addProduct: (product) => {},
-    reduceProudct: (product) => {},
-    clearCart: () => {},
+    addProduct: (product) => {
+        set((state) => {
+            //check if the product is present
+            const hasProduct = state.products.find((p) => p.id === product.id);
+            // calc the new total
+            const newTotal = +state.total.toFixed(2) + +product.price.toFixed(2);
+            // calc the count
+            const newCount = state.count + 1;
+
+            if (hasProduct) {
+                return {
+                    // if product already exists, increase quantity of that product by 1
+                    products: state.products.map((p) =>
+                        p.id === product.id ? { ...p, quantity: p.quantity + 1 } : p,
+                    ),
+                    total: newTotal,
+                    count: newCount,
+                }
+
+            } else {
+                return {
+                    //otherwise add a product with quantity of 1
+                    products: [...state.products, { ...product, quantity: 1 }],
+                    total: newTotal,
+                    count: newCount,
+                }
+            }
+        })
+    },
+    reduceProudct: (product) => {
+        set((state) => {
+            const newTotal = +state.total.toFixed(2) - +product.price.toFixed(2);
+            const newCount = state.count - 1;
+
+            return {
+                //return the new state of the products
+                products: state.products
+                    // if product matches the produc id subtract 1 from the quantity
+                    .map((p) => {
+                        if (p.id === product.id) {
+                            return { ...p, quantity: p.quantity - 1 };
+                        }
+                        return p;
+                    })
+                    .filter((p) => p.quantity > 0),
+
+                total: newTotal,
+                count: newCount,
+            }
+        })
+    },
+    clearCart: () => {
+        //reset the cart to the initial values
+        set(INITIAL_STATE);
+    },
 }))
 
 export default useCartStore;
