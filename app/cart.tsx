2722a56cbffae26831a244f549ee8b4d97765cf1
Daniel Sampson <sampsondg@outlook.com>
2025-04-02 18:31:06 -0300

    created cart page and populated with data from store

---
+++ app/cart.tsx
@@ -0,0 +1,79 @@
+import { Alert, FlatList, Platform, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import React from 'react'
+import useCartStore from '@/store/cartStore'
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { useRouter } from 'expo-router';
+import { Ionicons } from '@expo/vector-icons';
+import { COLORS } from '@/utils/colors';
+import CartItem from '@/components/CartItem';
+
+const Page = () => {
+    const { products, total, clearCart } = useCartStore();
+    const { bottom } = useSafeAreaInsets();
+    const router = useRouter();
+
+    const handleCheckout = () => {
+        if (products.length === 0) {
+            Alert.alert('Add some products to the cart first')
+            return;
+        }
+        clearCart();
+        Alert.alert('Checkout successful!')
+        router.dismiss();
+    }
+
+    return (
+        <View style={styles.container}>
+            {products.length === 0 && <Text style={styles.emptyText}>No products in cart!</Text>}
+            <FlatList
+                data={products}
+                renderItem={({ item }) => <CartItem item={item} />}
+                keyExtractor={(item) => item.id.toString()}
+                ListHeaderComponent={() => (<>
+                    {products.length > 0 ? (
+                        <Text style={styles.totalText}>Total: ${total}</Text>
+                    ) : null}
+                </>
+                )}
+            />
+
+            <TouchableOpacity style={[styles.addToCartButton, { paddingBottom: Platform.select({ ios: bottom, android: 15 }) }]} onPress={handleCheckout}>
+                <Ionicons name='checkmark' size={20} color={'#fff'} />
+                <Text style={styles.addToCartText}>Checkout</Text>
+            </TouchableOpacity>
+
+        </View>
+    )
+}
+
+export default Page
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+    },
+    emptyText: {
+        textAlign: 'center',
+    },
+    totalText: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        padding: 10,
+    },
+    addToCartButton: {
+        backgroundColor: COLORS.primary,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+        gap: 8,
+        padding: 16,
+    },
+    addToCartText: {
+        color: 'white',
+        fontWeight: '600',
+        fontSize: 16,
+    },
+
+})
