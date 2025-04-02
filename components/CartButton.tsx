16cdb4cdf30971fe7d24087b892c8eff9170d502
Daniel Sampson <sampsondg@outlook.com>
2025-04-02 17:31:37 -0300

    add touchable opacity for cart count as well as icon

--- components/CartButton.tsx
+++ components/CartButton.tsx
@@ -1,17 +1,44 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React from 'react'
 import useCartStore from '@/store/cartStore'
+import { Ionicons } from '@expo/vector-icons';
+import { COLORS } from '@/utils/colors';
 
 const CartButton = () => {
     const { count } = useCartStore(); // getting the count from our cart state
 
     return (
-        <View>
-            <Text>{count}</Text>
-        </View>
+        <TouchableOpacity>
+            {count > 0 && (
+                <View style={styles.countContainer}>
+                    <Text style={styles.countText}>{count}</Text>
+                </View>
+            )}
+            <Ionicons name='cart' size={28} color={'#000'} />
+        </TouchableOpacity>
     )
 }
 
 export default CartButton
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    countContainer: {
+        position: 'absolute',
+        right: -10,
+        bottom: -5,
+        backgroundColor: COLORS.primary,
+        borderRadius: 10,
+        alignItems: 'center',
+        justifyContent: 'center',
+        zIndex: 1,
+        width: 20,
+        height: 20,
+
+
+    },
+    countText: {
+        fontSize: 12,
+        color: '#fff',
+        fontWeight: 'bold',
+    }
+})
