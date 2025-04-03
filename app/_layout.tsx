013e964a38df093fda2c997b69f2a3815c25b7bc
Daniel Sampson <sampsondg@outlook.com>
2025-04-03 10:14:53 -0300

    add customization to Sentry setup

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,15 +2,36 @@ import CartButton from "@/components/CartButton";
 import { useMMKVDevTools } from "@dev-plugins/react-native-mmkv";
 import { useReactQueryDevTools } from "@dev-plugins/react-query";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { router, Stack } from "expo-router";
+import { router, Stack, useNavigationContainerRef, useRouter } from "expo-router";
 import { storage } from "@/store/mmkv";
 import { TouchableOpacity } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import * as Sentry from '@sentry/react-native';
+import { useEffect } from "react";
+
+const navigationIntegration = Sentry.reactNavigationIntegration({
+  enableTimeToInitialDisplay: true, // only in native builds and not in expo go
+})
 
 Sentry.init({
   dsn: 'https://03c3e963206190b6e4115dcb8af20d09@o4509089405927424.ingest.us.sentry.io/4509089416675328',
+  attachScreenshot: true,
+  debug: false,
+  tracesSampleRate: 1.0, // adjust to lower in productuion
+  _experiments: {
+    profileSampleRate: 1.0, // same as the other sample rate
+  },
+  integrations:[
+    Sentry.mobileReplayIntegration({
+      // these would be set to true to mask any user information
+      maskAllImages: false,
+      maskAllText: false,
+      maskAllVectors: false,
+    }),
+    Sentry.spotlightIntegration(),
+    navigationIntegration,
+  ]
 
   // uncomment the line below to enable Spotlight (https://spotlightjs.com)
   // spotlight: __DEV__,
@@ -29,6 +50,13 @@ export default Sentry.wrap(function RootLayout() {
   useMMKVDevTools({
     storage
   });
+
+  const router = useRouter();
+
+  const ref = useNavigationContainerRef();
+  useEffect(() => {
+    navigationIntegration.registerNavigationContainer(ref);
+  }, [ref])
 
   return (
     <QueryClientProvider client={queryClient}>
